54920777b0cef4115c8bc3fd88e0e9db94e0a418
frus71323 <frus71323@GFI541560.annuaire.groupe.local>
2018-02-13 12:04:47 +0100

    Started Project

---
+++ App/PointBreak/src/main/java/org/epsi/pointbreak/PointBreakApplication.java
@@ -0,0 +1,12 @@
+package org.epsi.pointbreak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointBreakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointBreakApplication.class, args);
+	}
+}
